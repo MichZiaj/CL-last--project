176381703a2dfb75eeed5a6ab2593e3deeb58a90
MichZiaj <mi.ziajkowski@gmail.com>
2021-11-23 13:36:31 +0100

    config

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            To jest strona O nas
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            To jest strona Kontaktu,
+        </div>
+    );
+};
+
+export default Contact;
