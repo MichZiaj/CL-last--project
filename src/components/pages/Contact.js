2c5e4a365d2bf7cec4ab3101dbdccf47b95831a4
MichZiaj <mi.ziajkowski@gmail.com>
2021-12-01 16:17:35 +0100

    Cennik icon

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -28,7 +28,7 @@ import emailjs from "emailjs-com";
                             <iframe
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d38775.01625719717!2d21.432219072744584!3d52.59784406449479!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ee50b7672a1bb%3A0x22977eae8b66ff03!2zxZp3acSZdG9qYcWEc2thIDE0OSwgMDctMjAyIFd5c3prw7N3!5e0!3m2!1spl!2spl!4v1638361328311!5m2!1spl!2spl"
                             style={{"width":"500px", "height":"350px", "border":"0"}} allowFullScreen="" loading="lazy"></iframe>
-                            <p className="address_text">Wyszków <br/> 07-202 <br/> ul. Świętojańska 149 <br/> Tel: <br/> Email: barbeerziza@gmail.com </p>
+                            <p className="address_text">Wyszków <br/> 07-202 <br/> ul. Świętojańska 149 <br/> Tel: 881-229-340 <br/> Email: barbeerziza@gmail.com </p>
                         </div>
 
 
