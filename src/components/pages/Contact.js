cbdd69c6c847d6679ef73b81be8fef69dce230b2
MichZiaj <mi.ziajkowski@gmail.com>
2021-12-01 14:24:34 +0100

    contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -24,36 +24,49 @@ import emailjs from "emailjs-com";
             <div className="container text__container">
                 <div className="contact_container">
 
-                    <div className="address">
+                        <div className="address">
+                            <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d38775.01625719717!2d21.432219072744584!3d52.59784406449479!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ee50b7672a1bb%3A0x22977eae8b66ff03!2zxZp3acSZdG9qYcWEc2thIDE0OSwgMDctMjAyIFd5c3prw7N3!5e0!3m2!1spl!2spl!4v1638361328311!5m2!1spl!2spl"
+                            style={{"width":"500px", "height":"350px", "border":"0"}} allowFullScreen="" loading="lazy"></iframe>
+                            <p className="address_text">Wyszków <br/> 07-202 <br/> ul. Świętojańska 149 <br/> Tel: <br/> Email: barbeerziza@gmail.com </p>
+                        </div>
 
 
 
 
-
-
-                    </div>
-
-
+                    <div className="send-email">
         <form ref={form} onSubmit={sendEmail}>
 
-            <label>Imię</label>
-            <input type="text" name="user_name" />
+            <div className="user-box">
+                <label>Imię</label>
+                <input type="text" name="user_name" />
+            </div>
 
-            <label>Nazwisko</label>
-            <input type="text" name="user_lastname" />
+            <div className="user-box">
+                <label>Nazwisko</label>
+                <input type="text" name="user_lastname" />
+            </div>
 
-            <label>Nr. tel.</label>
-            <input type="number" name="user_phone" />
+            <div className="user-box">
+                <label>Nr. tel.</label>
+                <input type="number" name="user_phone" />
+            </div>
 
-            <label>Email</label>
-            <input type="email" name="user_email" />
+            <div className="user-box">
+                <label>Email</label>
+                <input type="email" name="user_email" />
+            </div>
 
-            <label>Wiadomość</label>
-            <textarea name="message" />
+            <div className="user-message">
+                <label>Wiadomość</label>
+                <textarea name="message" />
+            </div>
 
-            <input type="submit" value="Send" />
+            <input  className="message-button" type="submit" value="Send" />
 
         </form>
+                    </div>
+
                 </div>
             </div>
         </section>
--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -8,6 +8,23 @@ import "swiper/swiper.scss"
 
 import SwiperCore, {EffectCoverflow,Pagination} from 'swiper';
 
+
+
+//Gallery photo import
+import photo1 from '../../images/Gallery/photo1.jpg';
+import photo2 from '../../images/Gallery/photo2.jpg';
+import photo3 from '../../images/Gallery/photo3.jpeg';
+import photo4 from '../../images/Gallery/photo4.jpeg';
+import photo5 from '../../images/Gallery/photo5.jpeg';
+import photo6 from '../../images/Gallery/photo6.jpg';
+import photo7 from '../../images/Gallery/photo7.jpeg';
+import photo8 from '../../images/Gallery/photo8.jpeg';
+import photo9 from '../../images/Gallery/photo9.jpeg';
+import photo10 from '../../images/Gallery/photo10.jpeg';
+import photo11 from '../../images/Gallery/photo11.jpeg';
+import photo12 from '../../images/Gallery/photo12.jpeg';
+
+
 SwiperCore.use([EffectCoverflow,Pagination]);
 
 
@@ -24,15 +41,19 @@ const Gallery = () => {
                 "modifier": 1,
                 "slideShadows": true
             }} pagination={true} className="mySwiper">
-                    <SwiperSlide><img  src="https://swiperjs.com/demos/images/nature-1.jpg" /></SwiperSlide>
-                    <SwiperSlide><img  src="https://swiperjs.com/demos/images/nature-2.jpg" /></SwiperSlide>
-                    <SwiperSlide><img  src="https://swiperjs.com/demos/images/nature-3.jpg" /></SwiperSlide>
-                    <SwiperSlide><img  src="https://swiperjs.com/demos/images/nature-4.jpg" /></SwiperSlide>
-                    <SwiperSlide><img  src="https://swiperjs.com/demos/images/nature-5.jpg" /></SwiperSlide>
-                    <SwiperSlide><img  src="https://swiperjs.com/demos/images/nature-6.jpg" /></SwiperSlide>
-                    <SwiperSlide><img  src="https://swiperjs.com/demos/images/nature-7.jpg" /></SwiperSlide>
-                    <SwiperSlide><img  src="https://swiperjs.com/demos/images/nature-8.jpg" /></SwiperSlide>
-                    <SwiperSlide><img  src="https://swiperjs.com/demos/images/nature-9.jpg" /></SwiperSlide>
+                    <SwiperSlide><img  src={photo1} alt="" /></SwiperSlide>
+                    <SwiperSlide><img  src={photo2} alt="" /></SwiperSlide>
+                    <SwiperSlide><img  src={photo3} alt="" /></SwiperSlide>
+                    <SwiperSlide><img  src={photo4} alt="" /></SwiperSlide>
+                    <SwiperSlide><img  src={photo5} alt="" /></SwiperSlide>
+                    <SwiperSlide><img  src={photo6} alt="" /></SwiperSlide>
+                    <SwiperSlide><img  src={photo7} alt="" /></SwiperSlide>
+                    <SwiperSlide><img  src={photo8} alt="" /></SwiperSlide>
+                    <SwiperSlide><img  src={photo9} alt="" /></SwiperSlide>
+                    <SwiperSlide><img  src={photo10} alt="" /></SwiperSlide>
+                    <SwiperSlide><img  src={photo11} alt="" /></SwiperSlide>
+                    <SwiperSlide><img  src={photo12} alt="" /></SwiperSlide>
+
                 </Swiper>
                     </div>
 
